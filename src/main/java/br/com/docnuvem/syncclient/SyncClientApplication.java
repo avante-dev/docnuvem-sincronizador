8109ef852af6f5845491db348c1a60fca9374724
Victor <victor@avanteempresas.com.br>
2026-03-16 14:21:11 -0300

    Initial commit

---
+++ src/main/java/br/com/docnuvem/syncclient/SyncClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.docnuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SyncClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SyncClientApplication.class, args);
+    }
+}
